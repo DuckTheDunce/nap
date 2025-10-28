e4a5fadde518b110020cc518e1270c7a7f337d67
DuckTheDunce <rodentiaschizophrenia@gmail.com>
2025-10-28 11:04:10 -0600

    uhh

--- src/mesh/geo.rs
+++ src/mesh/geo.rs
@@ -1,13 +1,37 @@
-struct Transformation{
-	matrix:[[i32; 3];3]
+use crate::render::{Pixel, Mask};
+
+pub struct Point{
+	x:i32,
+	y:i32,
+	z:i32
+}
+impl Point{
+	pub fn new(x:i32, y:i32, z:i32) -> Point{
+		return Point{x:x,y:y,z:z}
+	}
 }
 
-impl Transformation{
-	pub fn apply()
+pub struct Triangle{
+	points:[Point; 3],
+	x:i32,
+	y:i32,
+	z:i32
+}
+impl Triangle{
+	pub fn new(points:[Point;3], x:i32, y:i32, z:i32) -> Triangle{
+		return Triangle{points:points, x:x, y:y, z:z}
+	}
+
+	pub fn press(&self) -> Mask{
+		//get the line heights and widths, get the aperent size of the lines
+		let l1 = [self.points[0].x - self.points[1].x, self.points[0].y - self.points[1].y];
+
+	}
 }
 
-struct Rotation{
-	matrixX:[[f32;3];3],
-	matrixY:[[f32;3];3],
-	matrixZ:[[f32;3];3]
+pub struct Mesh{
+	triangles:Vec<Triangle>,
+	x:i32,
+	y:i32,
+	z:i32
 }
---
+++ src/mesh/mod.rs
@@ -0,0 +1,11 @@
+mod matrix;
+use matrix::Transform;
+
+mod geo;
+use geo::{Point, Triangle, Mesh};
+
+impl Point{
+	pub fn apply_transform(&mut self, matrix:Transform){
+
+	}
+}
