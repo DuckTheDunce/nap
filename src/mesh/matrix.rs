1126da91ef54bdd1e2291ca038892e39c65afeec
DuckTheDunce <rodentiaschizophrenia@gmail.com>
2025-10-31 11:01:01 -0600

    oh

--- src/mesh/matrix.rs
+++ src/mesh/matrix.rs
@@ -1,13 +1,48 @@
 struct Transformation{
-	matrix:[[i32; 3];3]
+	values:[[i32;4];4]
 }
 
-/*impl Transformation{
-	pub fn apply()
-}*/
+impl Transformation{
+    pub fn new() -> Transformation{
+		return Transformation{values:[[0, 0, 0, 0],[0, 0, 0, 0],[0, 0, 0, 0],[0, 0, 0, 0]]}
+    }
+	pub fn displace(&mut self, x:i32, y:i32, z:i32){
+		self.values[0][3] += x;
+		self.values[1][3] += y;
+		self.values[2][3] += z;
+	}
+	pub fn scale(&mut self, x:i32, y:i32, z:i32){
+		self.values[0][0] += x;
+		self.values[1][1] += y;
+		self.values[2][2] += z;
+	}
+}
+
+enum Direction{
+	X,
+	Y,
+	Z
+}
 
 struct Rotation{
-	matrixX:[[f32;3];3],
-	matrixY:[[f32;3];3],
-	matrixZ:[[f32;3];3]
+    values:[[f32;3];3]
+}
+
+impl Rotation{
+    pub fn new() -> Rotation{
+		return Rotation{values:[[0, 0, 0],[0, 0, 0],[0, 0, 0]]}
+    }
+	pub fn rotate(&mut self, direction:Direction, theta:f32){
+		match direction{
+			Direction::X => {
+				println!("ok");
+			}
+			Direction::Y => {
+				println!("ok");
+			}
+			Direction::Z => {
+				println!("ok");
+			}
+		}
+	}
 }
