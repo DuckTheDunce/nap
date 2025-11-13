a3e9710efd529678bd24c964f11824d65960b01b
DuckTheDunce <rodentiaschizophrenia@gmail.com>
2025-11-13 09:46:25 -0700

    little color

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,28 @@ mod point;
 mod tri;
 mod render;
 
+use colored::Colorize;
+
 fn main() {
-    let p1 = point::Point::new(9, 2, 0);
-    let p2 = point::Point::new(9, 0, 2);
-    let p3 = point::Point::new(2, 0, 0);
+    let p1 = point::Point::new(4, 0, 0);
+    let p2 = point::Point::new(40, 0, 40);
+    let p3 = point::Point::new(40, 40, 40);
 
     let t1 = tri::Triangle::new(p1, p2, p3);
 
 	let c_pos = point::Point::new(0, 0, 0);
 	let f_pos = point::Point::new(1, 0, 0);
-    let mut camera = render::Camera::new(c_pos, f_pos);
+    let camera = render::Camera::new(c_pos, f_pos, 5.0);
 
-    camera.render(vec![t1], 16, 16);
+    let t = camera.render(vec![t1], 16, 16);
+
+	let pixel_vec = t.pixels();
+	for i in 0..pixel_vec.len(){
+		if i%16 == 0{
+			println!();
+		}
+    	print!("{}", "o ".truecolor(pixel_vec[i].r, pixel_vec[i].g, pixel_vec[i].b));
+		
+	}
+    
 }
--- src/render.rs
+++ src/render.rs
@@ -2,17 +2,19 @@ use crate::point::Point;
 use crate::point;
 use crate::tri::Triangle;
 
-struct Pixel{
-	r:u8,
-	g:u8,
-	b:u8,
-	a:u8
+#[derive(Debug)]
+pub struct Pixel{
+	pub r:u8,
+	pub g:u8,
+	pub b:u8,
+	pub a:u8
 }
 
 impl Pixel{
 	pub fn new(r:u8, g:u8, b:u8, a:u8) -> Pixel{
 		return Pixel{r:r, g:g, b:b, a:a}
 	}
+	
 }
 
 pub struct PixelLayer{
@@ -20,37 +22,45 @@ pub struct PixelLayer{
 	width:u16
 }
 
+impl PixelLayer{
+	pub fn pixels(self) -> Vec<Pixel>{
+		return self.pixels;
+	}
+}
+
 pub struct Camera{
 	pos:Point,
-	focal_point:Point
+	focal_point:Point,
+	focal_scale:f32
 }
 
 impl Camera{
-    pub fn new(pos:Point, focal_point:Point) -> Camera{
-		return Camera{pos:pos, focal_point:focal_point}
+    pub fn new(pos:Point, focal_point:Point, focal_scale:f32) -> Camera{
+		return Camera{pos:pos, focal_point:focal_point, focal_scale:focal_scale}
     }
 	pub fn render(self, triangles:Vec<Triangle>, width:u32, height:u32) -> PixelLayer{
     	//find rotation of camera via focal point
 
-		let mut flat_points:Vec<[f32; 3]> = Vec::new();
+		let mut flat_points:Vec<[f32; 4]> = Vec::new();
     	
     	for triangle in triangles{
 			for point in triangle.get_points(){
 				let mut spher:[f32; 3] = point.spher();
+				let original_dist = spher[0];
 				spher[0] = 1.0;
-				flat_points.push(spher);
+				flat_points.push([spher[0],spher[1],spher[2],original_dist]);
 				//flat_points.push(point::sher_to_point(spher));
 			}
     	}
     	//get theta and azim range of camera
     	let focal_dist =  point::distance(self.pos, self.focal_point);
     	let offsets = self.focal_point.spher();
-    	let theta_range = (0.5/focal_dist as f32).atan();
-    	let azim_range = ((width as f32/(height as f32*2.0))/focal_dist as f32).atan();
+    	let theta_range = ((self.focal_scale)/focal_dist as f32).atan();
+    	let azim_range = ((width as f32/(height as f32*2.0))*self.focal_scale/focal_dist as f32).atan();
     	
 
     	let mut pix:Vec<Pixel> = Vec::new();
-    	for i in 0..(width*height){
+    	for _ in 0..(width*height){
 			pix.push(Pixel::new(0, 0, 0, 255));
     	}
 		//let mut 2d_points:Vec<[f32; 2]> = Vec::new();
@@ -62,8 +72,8 @@ impl Camera{
 			let azim = offsets[2] + azim_range;
 			let y_pos = ((azim - point[2])/(2.0*azim_range) * height as f32) as u32;
 			//2d_points.push([x_pos, y_pos]);
-			println!("x{} y{}", x_pos, y_pos);
-			pix[(x_pos + y_pos*width) as usize] = Pixel::new(255, 0, 0, 255);
+			println!("{}", point[3]);
+			pix[(x_pos + y_pos*width) as usize] = Pixel::new(255 - point[3] as u8, 255 - point[3] as u8, 255 - point[3] as u8, 255);
     	}
 
     	
